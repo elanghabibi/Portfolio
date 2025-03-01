7c41a2c890b15b530a44ad54ff8aef11f3804806
SkyLangg7 <154592963+SkyLangg7@users.noreply.github.com>
2025-03-01 11:46:33 +0700

    Delete assets directory

--- assets/js/preloader.js
+++
@@ -1,9 +0,0 @@
-window.onload = function() {
-    setTimeout(() => {
-        document.getElementById("preloader").style.opacity = "0";
-        setTimeout(() => {
-            document.getElementById("preloader").style.display = "none";
-            document.getElementById("content").classList.remove("hidden");
-        }, 500);
-    }, 1500); // Waktu animasi preloader (1.5 detik)
-};
